f0db3fcce1a58c5ca1d86355c23a9986e16c1f10
QuocBaoKho <baonhq2603@gmail.com>
2025-05-27 20:43:05 +0700

    First commit ò baokho

---
+++ src/main/java/com/example/hotelmanagement/Views/EditRoomTypeController.java
@@ -0,0 +1,17 @@
+package com.example.hotelmanagement.Views;
+
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EditRoomTypeController implements Initializable {
+    public ImageView roomTypeImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        roomTypeImageView.setImage(new Image(getClass().getResource("/Images/phong.jpg").toExternalForm()));
+    }
+}
